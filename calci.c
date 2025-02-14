d6832101fcbdb9f699a7aeb3455a88dc26c3aa3b
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-02-14 12:24:12 +0530

    subtraction is added to file

--- calci.c
+++ calci.c
@@ -5,6 +5,7 @@ int main() {
     int b = 20;
     int sum = a + b;
     printf("The sum of %d and %d is %d\n", a, b, sum);
-    
+   int sub=a - b;
+   printf("The sub of %d and %d is %d\n", a, b, sub);
 
 }
