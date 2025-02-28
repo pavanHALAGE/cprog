626a9a22989ee464d01a686cac09db8339752121
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-02-28 12:07:12 +0530

    3add is added to pyfile

---
+++ python.py
@@ -0,0 +1,10 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+
+total = num1 + num2 + num3
+
+
+print("The sum of the three numbers is:", total)
