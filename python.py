c53b6e05e50069a9c7c60e5e8198755f478d131b
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-02-28 12:24:42 +0530

    3division is added to pyfile

--- python.py
+++ python.py
@@ -7,6 +7,15 @@ num3 = float(input("Enter the third number: "))
 total = num1 + num2 + num3
 difference = num1 - num2 - num3
 product = num1 * num2 * num3
+if num2 != 0 and num3 != 0:
+    division = num1 / num2 / num3
+    floor_division = num1 // num2 // num3  # Floor division
+else:
+    division = "Undefined (cannot divide by zero)"
+    floor_division = "Undefined (cannot perform floor division by zero)"
+
+
 print("The sum of the three numbers is:", total)
 print("Subtraction result:", difference)
 print("Multiplication result:", product)
+print("Division result:", division)
