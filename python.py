7a1f69ac84a8f17dc22b27c094126c343aa532b9
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-02-28 12:22:42 +0530

    3multiplication is added to pyfile

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ num3 = float(input("Enter the third number: "))
 
 total = num1 + num2 + num3
 difference = num1 - num2 - num3
-
+product = num1 * num2 * num3
 print("The sum of the three numbers is:", total)
 print("Subtraction result:", difference)
-
+print("Multiplication result:", product)
