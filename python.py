cf989da7dcf35250b742450abc54d98d13b5390e
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-02-28 12:19:36 +0530

    3subtraction is added to pyfile

--- python.py
+++ python.py
@@ -5,6 +5,8 @@ num3 = float(input("Enter the third number: "))
 
 
 total = num1 + num2 + num3
-
+difference = num1 - num2 - num3
 
 print("The sum of the three numbers is:", total)
+print("Subtraction result:", difference)
+
