12753a166cddeb7a225009d24c9ddd433a8fa713
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-21 22:21:53 +0530

    intersection lof set is added

--- py.py
+++ py.py
@@ -2,3 +2,6 @@ set_a = {1, 2, 3, 4}
 set_b = {3, 4, 5, 6}
 union_set = set_a.union(set_b)
 print("Union:", union_set)
+
+intersection_set = set_a.intersection(set_b)
+print("Intersection:", intersection_set)
