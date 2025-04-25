aabc992f77abb90e04b41087abd02bf49f67615f
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-25 21:44:17 +0530

    string is splited

--- string.py
+++ string.py
@@ -11,3 +11,5 @@ print("Length of string:", len(message))
 print("Replace 'World' with 'Python':", message.replace("World", "Python"))
 
 print("Check if 'Hello' is in the string:", "Hello" in message)
+
+print("Split the string by comma:", message.split(","))
