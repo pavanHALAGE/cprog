fca5b474dbb6770f7d0b95d1be8ff14cbabcc75e
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-25 21:41:53 +0530

    string is replaced

--- string.py
+++ string.py
@@ -7,3 +7,5 @@ print("Uppercase:", message.upper())
 print("Lowercase:", message.lower())
 
 print("Length of string:", len(message))
+
+print("Replace 'World' with 'Python':", message.replace("World", "Python"))
