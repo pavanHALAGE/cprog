956f5cc3a5c07056c5f87a9920501bc10bbc6f3f
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-27 22:03:52 +0530

    dictionary student college is added

--- d.py
+++ d.py
@@ -9,3 +9,8 @@ print("Original Dictionary:", student)
 print("\nAccessing 'name':", student['name'])
 
 print("Accessing 'age':", student.get('age'))
+
+student['college'] = "NIT Karnataka"
+print("\nAfter Adding 'college':", student)
+
+
