3c7dc2c7a2bd8f8d8ab25e78b870d948c3f0476f
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-29 19:43:34 +0530

    num 2 is divisble by 2 is checked

--- cp.c
+++ cp.c
@@ -35,5 +35,11 @@ if (num1 != 0 && num2 % num1 == 0)
         printf("%d is divisible by %d\n", num2, num1);
     else if (num1 != 0)
         printf("%d is not divisible by %d\n", num2, num1);
+if (num2 != 0 && num1 % num2 == 0)
+        printf("%d is divisible by %d\n", num1, num2);
+    else if (num2 != 0)
+        printf("%d is not divisible by %d\n", num1, num2);
 
+	return 0;
+}
 
