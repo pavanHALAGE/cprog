7bd78512204ba94674f96e8bc31f57678bd82b1e
pavanHALAGE <pavan.hegde@s.amity.edu>
2025-04-29 19:29:07 +0530

    1st number even is checked

--- cp.c
+++ cp.c
@@ -2,3 +2,13 @@
 
 int main() {
     int num1, num2;
+
+    printf("Enter two integers:\n");
+    scanf("%d %d", &num1, &num2);
+
+    
+    if (num1 % 2 == 0)
+        printf("%d is Even\n", num1);
+
+
+
